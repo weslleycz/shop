9834f6d11b73001bd9ea43cdd5471c31f4a248f5
Francisco Weslley <weslleysouza22@gmail.com>
2022-09-08 00:04:08 -0300

    🧱 Estrutura do projeto.

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    app.use(cors());
+    next();
+});
+
+app.use(express.json({ limit: "200mb" }));
+
+app.listen(process.env.PORT, () => {
+    console.log(`🚀 Server started on port:${process.env.PORT}`);
+});
